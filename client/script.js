5958637c4195b92dd8d30be74a6c28694247cbf7
Drew Charles <williamcharles386@gmail.com>
2023-02-13 16:53:07 -0500

    update the server url

--- client/script.js
+++ client/script.js
@@ -79,7 +79,7 @@ const handleSubmit = async (e) => {
 
   //fetch data from server
 
-  const response = await fetch('http://localhost:5000/', {
+  const response = await fetch('https://codex-nzqr.onrender.com', {
     method: 'POST',
     headers: {
         'Content-Type': 'application/json',
